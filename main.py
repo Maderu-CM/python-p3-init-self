e59196c9ec9c6ec41d950b5b1ae63a7ff880214f
Maderu-CM <clive.moyia@student.moringaschool.com>
2023-09-08 01:56:06 +0300

    Assignmment Done

---
+++ main.py
@@ -0,0 +1,10 @@
+from lib.dog import Dog
+from lib.person import Person
+
+# Create a Dog object
+my_dog = Dog("Rex", "German Shepherd")
+print(f"My dog's name is {my_dog.name} and it's a {my_dog.breed}.")
+
+# Create a Person object
+my_person = Person("Clive")
+print(f"My name is {my_person.name}.")
